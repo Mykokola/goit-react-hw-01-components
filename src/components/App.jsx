e3cea1039dd49520653d6b8cca7c4b31fe3c5ead
Mykokola <j0kfrbr@gmail.com>
2023-05-22 15:48:19 -0500

    add 2 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,17 @@ import Profile from './Profile/Profile';
 import user from './Profile/user.json';
 import Statistic from './Statistic/Statistic';
 import data from './Statistic/data.json'
-console.log(data)
 export const App = () => {
   return (
+    <div>
     <Profile
     username={user.username}
     tag={user.tag}
     location={user.location}
     avatar={user.avatar}
     stats={user.stats}
-  />
+/>  
+<Statistic title='Upload stats' stats={data}/>
+  </div>
   );
 };
--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -1,29 +1,41 @@
 import PropTypes from 'prop-types';
-const Statistic = ({}) => {
+import {
+  StatisticSection,
+  StatisticTitle,
+  StatisticList,
+  StatisticListItem,
+} from './Statistic.styled';
+const Statistic = ({ stats, title }) => {
+  return (
+    <StatisticSection class="statistics">
+      <StatisticTitle class="title">{title || 'Upload stats'}</StatisticTitle>
+      <StatisticList class="stat-list">
+        {stats.map(e => (
+  
+  <StatisticListItem style={{background:rundomColorGenerator()}} key={e.id} class="item">
+            <span class="label">{e.label}</span>
+            <span class="percentage">{e.percentage}%</span>
+          </StatisticListItem>
+        ))}
+      </StatisticList>
+    </StatisticSection>
+  );
+};
 
-    return (
-        <section class="statistics">
-  <h2 class="title">Upload stats</h2>
-
-  <ul class="stat-list">
-    <li class="item">
-      <span class="label">.docx</span>
-      <span class="percentage">4%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp3</span>
-      <span class="percentage">14%</span>
-    </li>
-    <li class="item">
-      <span class="label">.pdf</span>
-      <span class="percentage">41%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp4</span>
-      <span class="percentage">12%</span>
-    </li>
-  </ul>
-</section>
-    )
-}
-export default Statistic
+Statistic.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+function rundomColorGenerator() {
+    let r = Math.floor(Math.random() * 256),
+      g = Math.floor(Math.random() * 256),
+      b = Math.floor(Math.random() * 256);
+    return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+  }
+  
+export default Statistic;
